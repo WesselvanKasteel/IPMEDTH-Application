a0cb4487d5f524577ccbd3afd24ce645d12358fd
Wessel van Kasteel <s1115662@student.hsleiden.nl>
2022-11-27 17:43:20 +0100

    Added line drawer script

---
+++ Assets/ProjectAssets/Scripts/DevLineDrawer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevLineDrawer : MonoBehaviour
+{
+
+    public LineRenderer line;
+
+    public Transform worldOrigin;
+    public Transform objectOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPosition(0, worldOrigin.position);
+        line.SetPosition(1, objectOrigin.position);
+    }
+}
